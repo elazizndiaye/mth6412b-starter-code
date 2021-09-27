1de62979dabc3f9438aa7492c71359e25528d60e
elazizndiaye <azibdndiaye@gmail.com>
2021-09-27 13:39:52 -0400

    initialisation du rapport.

---
+++ projet/phase1/rapportPhase1.jl
@@ -0,0 +1,31 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ a47e8762-1fb5-11ec-390e-41ee161c06cc
+md"
+# _MTH6412b: Projet voyageur de commerce_
+## Phase 1
+Équipe:
+
+	El Hadji Abdou Aziz NDIAYE (1879468)
+	Paul Patience 			 (1234567)
+"
+
+# ╔═╡ a5d6aed0-3798-468a-8539-10c30d2e55ee
+md"
+### Type Edge
+Le type edge permet de représenter les arêtes d'un graph. Chaque objet de type _Edge_ est composé de trois champs: `name`, `nodes` et `weight`.
+
+Le champ `name` est une chaine de caractère de type String comportant le nom de l'arête.
+
+Le champ `nodes` est un vecteur de deux éléments de type _Node_ correspondant aux noeuds de l'arête. 
+
+Le champ `weight` est un nombre qui représente le poids de l'arête.
+"
+
+# ╔═╡ Cell order:
+# ╠═a47e8762-1fb5-11ec-390e-41ee161c06cc
+# ╠═a5d6aed0-3798-468a-8539-10c30d2e55ee
