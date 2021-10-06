e90ed68663afb0f55cd66f8b3907c289ec66b0b5
elazizndiaye <azibdndiaye@gmail.com>
2021-10-05 23:28:56 -0400

    Initialisation du rapport de la phase 2.

---
+++ projet/phase2/rapport_phase2.jl
@@ -0,0 +1,50 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 06e93d60-2654-11ec-17fe-89d9d50370a9
+md"
+# MTH6412B: Projet Voyageur de Commerce
+## Phase 2: Arbre de recouvrement minimal
+* El Hadji Abdou Aziz Ndiaye (1879468)
+* Le code source se trouve à l'addresse 
+"
+
+# ╔═╡ 975e877e-da60-4e5e-837f-7826c08ecd5f
+md"
+## Importation du code
+"
+
+# ╔═╡ f2622aef-9953-41c6-93c3-fe3576806e88
+md"
+## Révision du code de la phase 1
+"
+
+# ╔═╡ dba5c731-b588-4b74-9298-b2c8935b2222
+md"
+## Structure de données des composantes connexes
+### Implémentation
+### Tests
+"
+
+# ╔═╡ 4039cb69-0108-4d00-9754-d7301d501368
+md"
+## Algorithme de Kruskal
+### Implémentation
+### Test
+"
+
+# ╔═╡ 095a38af-8e8b-46f8-9114-53864b9d6136
+md"
+## Programme principal
+"
+
+# ╔═╡ Cell order:
+# ╟─06e93d60-2654-11ec-17fe-89d9d50370a9
+# ╟─975e877e-da60-4e5e-837f-7826c08ecd5f
+# ╟─f2622aef-9953-41c6-93c3-fe3576806e88
+# ╟─dba5c731-b588-4b74-9298-b2c8935b2222
+# ╟─4039cb69-0108-4d00-9754-d7301d501368
+# ╟─095a38af-8e8b-46f8-9114-53864b9d6136
