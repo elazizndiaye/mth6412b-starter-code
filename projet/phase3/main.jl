c61c8f8b7a06e15d446361fb11e434340e1ed051
elazizndiaye <azibdndiaye@gmail.com>
2021-10-30 15:28:28 -0400

    programme principal phase 3

--- projet/phase3/main.jl
+++ projet/phase3/main.jl
@@ -3,7 +3,8 @@ include("node.jl");
 include("edge.jl");
 include("read_stsp.jl");
 include("graph.jl");
-include("min_span_tree.jl")
+include("mst_kruskal.jl")
+include("mst_prim.jl")
 
 """ Programme principal. """
 function main()
@@ -15,8 +16,10 @@ function main()
             graph = stsp_to_graph(joinpath(root, file);show_graph_flag=showgraph,plot_graph_flag=plotgraph);
             # Arbre de recouvrement minimal du graph
             print("Arbre de recouvrement minimal : ")
-            mst = kruskal(graph);
-            println("$(weight_mst(mst)) (poids total) -v");
+            mst_kruskal = kruskal(graph);
+            print("$(weight_mst(mst_kruskal)) (Kruskal) ");
+            mst_prim = prim(graph);
+            println("$(weight_mst(mst_prim)) (Prim) -v");
         end
     end
     
