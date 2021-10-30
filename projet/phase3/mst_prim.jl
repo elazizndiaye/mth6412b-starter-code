0b379fdce6ff4ebf7f94f5c0f0eb5b8c66d424e3
elazizndiaye <azibdndiaye@gmail.com>
2021-10-30 15:18:22 -0400

    implémentation complete de l'algorithme de Prim

--- projet/phase3/mst_prim.jl
+++ projet/phase3/mst_prim.jl
@@ -1,4 +1,5 @@
-import Base.show
+import Base.show, Base.isless
+include("queue.jl")
 
 """Type representant un noeud d'une composante connexe d'un graphe."""
 mutable struct ComponentPrim{T}
@@ -10,108 +11,68 @@ end
 """Construit une composante connexe à partir d'un noeud."""
 ComponentPrim(node::Node{T}) where T = ComponentPrim{T}(node, nothing, Inf);
 
-"""Retourne le noeud parent d'un noeud-composante."""
-parent(compo::Component{T}) where T = compo.parent;
+"""Retourne le parent d'un noeud-composante."""
+parent(compo::ComponentPrim{T}) where T = compo.parent;
 
 """Retourne le noeud d'un noeud-composante."""
-node(compo::Component{T}) where T = compo.node;
+node(compo::ComponentPrim{T}) where T = compo.node;
 
-"""Retourne le rang d'un noeud-composante."""
-rank(compo::Component{T}) where T = compo.rank;
+"""Retourne le poids d'un noeud-composante."""
+min_weight(compo::ComponentPrim{T}) where T = compo.min_weight;
 
-"""Mute le noeud parent d'un noeud-composante."""
-function set_parent!(compo::Component{T}, parent::Node{T}) where T 
+"""Mute le parent d'un noeud-composante."""
+function set_parent!(compo::ComponentPrim{T}, parent) where T 
     compo.parent = parent;
 end
 
-"""Incrémente le rang d'un noeud-composante."""
-function increment_rank!(compo::Component{T}) where T 
-    compo.rank = compo.rank + 1;
+"""Mute le poids d'un noeud-composante."""
+function set_min_weight!(compo::ComponentPrim{T}, min_weight) where T 
+    compo.min_weight = min_weight;
 end
 
-"""Type représentant l'ensemble des composantes connexes d'un graphe."""
-mutable struct ConnectedComponents{T}
-    components::Dict{Node{T}}{Component{T}}
-end
-
-"""Construit un ensemble de composantes connexes à partir d'un vecteur de noeud-composante."""
-function ConnectedComponents(compos::Vector{Component{T}}) where T 
-   connec_compos = ConnectedComponents(Dict{Node{T},Component{T}}());
-   for icompo = 1:length(compos)
-    add_component!(connec_compos, compos[icompo]);
-   end
-   connec_compos
-end
-
-"""Ajoute un noeud-composante dans l'ensemble des composante des composantes connexes."""
-function add_component!(connec_compos::ConnectedComponents{T}, compo::Component{T}) where T
-    node_ = node(compo);
-    connec_compos.components[node_] = compo;
-    return connec_compos
-end
-
-"""Retourne un noeud-composante à partir d'un noeud."""
-function component(connec_compos::ConnectedComponents{T}, node::Node{T}) where T
-    compo = connec_compos.components[node];
-    return compo
-end
-
-"""Retourne la racine d'un noeud."""
-function find_root(connec_compos::ConnectedComponents{T}, node_::Node{T}) where T
-    compo = component(connec_compos, node_);
-    parent_ = parent(compo);
-    compos_encountred = Vector{Component{T}}();
-    while parent_ != node_
-        push!(compos_encountred, compo);
-        node_ = parent_;
-        compo = component(connec_compos, node_);
-        parent_ = parent(compo);
-    end
-    # Compression des chemins
-    for compo_encountred in compos_encountred
-        set_parent!(compo_encountred, node_);
-    end
-    node_
-end
-
-"""Fusionne deux composantes connexes."""
-function union_components!(connec_compos::ConnectedComponents{T}, node1::Node{T}, node2::Node{T}) where T
-    root1 = find_root(connec_compos, node1); compo1 = component(connec_compos, root1);
-    root2 = find_root(connec_compos, node2); compo2 = component(connec_compos, root2);
-    # union via le rang
-    if rank(compo2) > rank(compo1)
-        set_parent!(compo1, root2);
-    elseif rank(compo2) < rank(compo1)
-        set_parent!(compo2, root1);
-    else
-        set_parent!(compo1, root2);
-        increment_rank!(compo2);
-end
-end
+"""Compare deux noeud-composantes: inégalité stricte."""
+isless(compo1::ComponentPrim{T},compo2::ComponentPrim{T}) where T = min_weight(compo1) < min_weight(compo2);
 
 """Retourne l'arbre de recouvrement minimal en utilisant l'algorithme de Prim."""
-function prim(graph::AbstractGraph, node_source::Node{T})
-    # Initialisation des composantes connexes
-    connec_compos = ConnectedComponents(Component{Int}[]);
+function prim(graph::AbstractGraph; node_source=nothing)
+    # Initialisation des noeud-composantes
+    compos = Dict{Node{Int},ComponentPrim{Int}}();
+    priorityQ = PriorityQueue{ComponentPrim{Int}}()
     nodes_ = nodes(graph);
     nb_nodes_ = nb_nodes(graph);
     for inode = 1:nb_nodes_
-        compo = Component(nodes_[inode]);
-        add_component!(connec_compos, compo);
+        compo = ComponentPrim(nodes_[inode]);
+        compos[nodes_[inode]] = compo;
+        push!(priorityQ, compo);
     end
-    set_min_weight!(connec_compos, node_source, 0);
+    if node_source === nothing
+        node_source = nodes_[1];
+    end
+    set_min_weight!(compos[node_source], 0);
+    # Calcul des listes d'adjacence
+    adj_edges_graph_start, adj_edges_graph = node_to_edges(graph);
     # Construction de l'arbre de recouvrement minimal
     mst = Edge{Int}[]
-    nb_edges_ = nb_edges(graph)
-    for iedge = 1:nb_edges_
-        edge = sorted_edges[iedge];
-        node1 = start_node(edge);
-        node2 = end_node(edge);
-        root1 = find_root(connec_compos, node1);
-        root2 = find_root(connec_compos, node2);
-        if root1 != root2
-            push!(mst, edge);
-            union_components!(connec_compos, root1, root2);
+    while !is_empty(priorityQ)
+        compo = popfirst!(priorityQ);
+        current_node = node(compo);
+        parent_compo = parent(compo);
+        if parent_compo !== nothing
+            push!(mst, parent_compo);
+        end
+        # extraction des arêtes adjacents
+        istart = data(current_node);
+        adj_edges = adj_edges_graph[adj_edges_graph_start[istart]:adj_edges_graph_start[istart + 1] - 1];
+        for edge in adj_edges
+            other_node = start_node(edge);
+            if other_node == current_node
+                other_node = end_node(edge);
+            end
+            other_compo = compos[other_node];
+            if in_queue(priorityQ, other_compo) && weight(edge) < min_weight(other_compo)
+                set_parent!(other_compo, edge);
+                set_min_weight!(other_compo, weight(edge));
+            end
         end
     end
     return mst
